5804bde7a2f0369f18a249110c9553318a7ab4cc
craigferrere <cferrere@law.harvard.edu>
2025-07-08 18:11:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -559,6 +559,8 @@ if page == "Dashboard":
                     "professor at a [top (5), 1st tier (6-20), 2nd tier (21-50), 3rd tier (50 and under), unranked, European (including UK), non-US, top European (Oxford or Cambridge), top non-US] university (<affiliation>)"
                 )
                 
+               affiliations = paper.get("affiliations", []) 
+                
                 author_descriptions = [
                     f"{name} is a {st.session_state.get(f'status_selected_{name}')} {st.session_state.get(f'field_selected_{name}')} professor at {descriptor}"
                     for name, affiliation in zip(last_names, affiliations)
