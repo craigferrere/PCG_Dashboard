f7af5e10718fd38331567c2d9eb0715db552850e
craigferrere <cferrere@law.harvard.edu>
2025-07-09 11:38:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -638,7 +638,7 @@ if page == "Dashboard":
                         "<atoniolo@corpgov.law.harvard.edu>")
             st.markdown(f"**Subject:** {subject}")
 
-            if not st.session_state.get("manual_email_edit"):
+            if st.session_state.get("manual_email_edit"):
                 current_text = st.session_state.get("manual_email_text") or draft_body
                 new_text = st.text_area("Edit Email Body", value=current_text, height=300, key="editable_text_area")
                 if st.button("Save and Exit Edit Mode (Ctrl+Enter)", key="save_manual_email_text"):
